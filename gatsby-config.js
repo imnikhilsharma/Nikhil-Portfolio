c6955209b9a6c6c98fb64642a2a2622edffe932e
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-11-16 19:27:34 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       options: {
         name: 'Cara - @lekoarts/gatsby-theme-cara',
         short_name: 'Cara',
-        description: 'Looking for Tachnical SEO Auditor that help to boost your presence Google Search Engine',
+        description: 'Looking for Tachnical SEO Auditor that help to boost your presence Google Search Engine. Get In touch with met at:+91-9588066361',
         start_url: '/',
         background_color: '#141821',
         theme_color: '#f6ad55',
