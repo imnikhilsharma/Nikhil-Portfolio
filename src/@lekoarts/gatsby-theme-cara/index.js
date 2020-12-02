37f4e11aa151aae60391f1f8953399ccb942ed8f
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-12-03 00:04:51 +0530

    Delete index.js

--- src/@lekoarts/gatsby-theme-cara/index.js
+++
@@ -1,7 +0,0 @@
-<Helmet
-    title={data.site.siteMetadata.title}
-    meta={[
-    { name: 'description', content: 'Your description here' },
-    { name: 'google-site-verification', content: 'WUIwqD5MY-N_Aa8TtmHUcvlDIzpca83qYc4ffBbMD5A' }
-    ]}
-/>
