56e4e51ed8d1988ff9b2185920c0b9f4e350893c
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-12-02 23:39:50 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
     resolve: `gatsby-plugin-canonical-urls`,
     options: {
-      siteUrl:'https://nikhil-sharma.netlify.app/',
+      siteUrl:'https://nikhilsharma.gtsb.io/',
     }
     }, 
     {
