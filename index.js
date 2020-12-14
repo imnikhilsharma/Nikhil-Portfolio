793834eb9142ea08520e42cedfe7713763875a8f
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-12-14 23:39:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
-import Helmet from 'react-helmet',
+import Helmet from 'react-helmet';
 <Helmet>
   <html lang="en" />
   <title>{site.siteMetadata.title}</title>
  <meta name="google-site-verification" content="WUIwqD5MY-N_Aa8TtmHUcvlDIzpca83qYc4ffBbMD5A"/>
-</Helmet>,
+</Helmet>
