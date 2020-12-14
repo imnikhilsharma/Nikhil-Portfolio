66e622acfada93e216f38de17fa6b12d019c44b1
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-12-14 22:27:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+//index.js
+import Helmet from 'react-helmet';
+<Helmet>
+<html lang="en" />
+<meta name="google-site-verification" content="WUIwqD5MY-N_Aa8TtmHUcvlDIzpca83qYc4ffBbMD5A" />
+</Helmet>
